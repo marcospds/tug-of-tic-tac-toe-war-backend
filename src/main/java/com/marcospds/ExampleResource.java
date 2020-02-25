be7c3a314dfe06769659e6890c73fc2f521afe8c
Marcos Paulo <marcos.paulo@totvs.com.br>
2020-02-25 09:33:09 -0300

    Update application.properties

--- src/main/java/com/marcospds/ExampleResource.java
+++ src/main/java/com/marcospds/ExampleResource.java
@@ -5,12 +5,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import io.vertx.core.json.Json;
+
 @Path("/hello")
 public class ExampleResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     public String hello() {
-        return "hello";
+    	
+        return "[{\"name\":\"eaaaaarl of Lemongrab\",\"age\":\"Unknown\",\"species\":\"Lemon Candy\",\"occupation\":\"Earl, Heir to the Candy Kingdom Throne\"},{\"name\":\"Bonnibel Bubblegum\",\"age\":\"19\",\"species\":\"Gum Person\",\"occupation\":\"Returned Ruler of the Candy Kingdom\"}]";
     }
 }
